aa07d5b2adf00a4ee31c8920b15d338fa0821fe3
Erick <19063989@student.hhs.nl>
2024-04-16 20:36:42 +0200

    Added player movement

--- Assets/Game_Manager.cs
+++ Assets/Game_Manager.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Game_Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
+    private Vector2 vector;
+    private Vector3 moveDirection;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
     }
+
+    void Movement()
+    {
+        moveDirection = new Vector3(vector.x, 0f, vector.y);
+        transform.Translate(moveSpeed * Time.deltaTime * moveDirection, Space.World);
+    }
+
+    void OnMove(InputValue value) => vector = value.Get<Vector2>();
 }
