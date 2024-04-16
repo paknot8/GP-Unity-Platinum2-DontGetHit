1efd5bad97dbca676f46380e17d79dc8e9e3aaad
Erick <19063989@student.hhs.nl>
2024-04-16 19:10:14 +0200

    added button clicks and scripts

--- Assets/UI_Manager.cs
+++ Assets/UI_Manager.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MainMenuUIManager : MonoBehaviour
+public class UI_Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject mainMenuPanel;
+    public GameObject settingsPanel;
+
     void Start()
     {
-        
+        // Ensure that the main menu panel is active and the settings panel is inactive at the start
+        mainMenuPanel.SetActive(true);
+        settingsPanel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayGame()
     {
-        
+        // Load scene 1
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void EnableSettings()
+    {
+        // Enable settings panel and disable main menu panel
+        settingsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+    }
+
+    public void EnableMainMenu()
+    {
+        // Enable main menu panel and disable settings panel
+        mainMenuPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Quit editor play mode
+#else
+        Application.Quit(); // Quit standalone build
+#endif
     }
 }
