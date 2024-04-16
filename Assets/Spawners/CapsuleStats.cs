3e078641e33b54e268443042561d832facf942cd
Erick <19063989@student.hhs.nl>
2024-04-16 21:16:00 +0200

    Added spawners

---
+++ Assets/Spawners/CapsuleStats.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CapsuleStats : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Speed at which the object moves
+    public float destroyPosition = 10f; // Position at which the object should be destroyed
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveAndDestroy();
+    }
+
+    void MoveAndDestroy()
+    {
+        transform.Translate(moveSpeed * Time.deltaTime * Vector3.left);
+
+        // Check if the object has reached the destroy position
+        if (transform.position.x >= destroyPosition)
+        {
+            // Destroy the object
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Spawners/Spawner3.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Spawner3 : MonoBehaviour
+{
+    public GameObject spawnPoint;
+    public GameObject objectToSpawn; // Prefab to spawn
+
+    private float spawnTimer = 0f;
+    private readonly float spawnInterval = 1f; // Time interval between spawns
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnObjects();
+    }
+
+    void SpawnObjects()
+    {
+        // Increment the timer
+        spawnTimer += Time.deltaTime;
+
+        // Check if it's time to spawn a new object
+        if (spawnTimer >= spawnInterval)
+        {
+            // Reset the timer
+            spawnTimer = 0f;
+
+            // Spawn a new object at the spawn point
+            if(objectToSpawn != null){
+                Instantiate(objectToSpawn, spawnPoint.transform.position, Quaternion.identity);
+            }
+        }
+    }
+}
